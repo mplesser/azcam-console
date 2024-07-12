31a703dee33990e6faaed10578297d33cc23fbb8
Michael Lesser <mlesser@arizona.edu>
2024-07-12 12:46:36 -0700

    update

--- support/start_observe.bat
+++ support/start_observe.bat
@@ -1,7 +1,3 @@
-rem PowerShell -WindowStyle Hidden c:\azcam\azcam-observe\azcam_observe\observe.pyw -port 2402
+python -m azcam_console.observe - -port 2402
 
-rem PowerShell -WindowStyle Hidden "python -m observe.pyw -port 2402"
-
-rem python -m azcam_observe.observe - -port 2402
-
-rem PowerShell -WindowStyle Hidden "python -m azcam_observe.start - -port 2402"
+rem PowerShell -WindowStyle Hidden "python -m azcam_console.observe - -port 2402"
